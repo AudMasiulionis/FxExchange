343d290a9b4ac89d70e6bfa6d80f10a559d6824e
Audrius Masiulionis <audrius.masiulionis@visma.com>
2019-03-12 18:24:37 +0200

    Add project files.

---
+++ FxExchange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FxExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
